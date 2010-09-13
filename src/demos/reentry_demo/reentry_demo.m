b66837f285a41720735c634d5e2efe2845ccd985
Arno Solin <arno.solin@aalto.fi>
2010-09-13 13:09:16 +0000

    Added GHKF and CKF demos.
    git-svn-id: file:///proj/bayes/svn/toolbox/repos/ukf_ekf_toolbox/trunk@441 09c0cf2a-6a2c-0410-9c27-f7251c85c0f3

--- src/demos/reentry_demo/reentry_demo.m
+++ src/demos/reentry_demo/reentry_demo.m
@@ -1,12 +1,34 @@
-% Discrete-time reentry dynamics demonstration with EKF and UKF. 
-
-% Copyright (C) 2006 Simo S‰rkk‰
-%               2007 Jouni Hartikainen
+%% Discrete-time reentry dynamics demonstration with non-linear filters
 %
-% This software is distributed under the GNU General Public 
-% Licence (version 2 or later); please refer to the file 
-% Licence.txt, included with the software, for details.
-  function reentry_demo
+%  Description:
+%    In this example various different non-linear filters and smoothers are
+%    applied to reentry tracking problem . The filters used in this 
+%    demonstration are:
+%      * Extended Kalman filter
+%      * Unscented Kalman filter
+%      * Gauss-Hermite Kalman filter (degree 3)
+%      * Cubature Kalman filter
+%    Additionally, the corresponding smoother results are also presented.
+%
+%  References:
+%    Refer to the Toolbox documentation for details on the model.
+%
+%  See also:
+%    ukf_predict1, ukf_update1, urts_smooth1,
+%    ekf_predict1, ekf_update1, erts_smooth1, ghkf_predict, ghkf_update, 
+%    ghrts_smooth, ckf_predict, ckf_update, crts_smooth
+%
+%  Author:
+%    Copyright (C) 2006 Simo S√§rkk√§
+%                  2007 Jouni Hartikainen
+%                  2010 Arno Solin
+%
+%  Licence:
+%    This software is distributed under the GNU General Public 
+%    Licence (version 2 or later); please refer to the file 
+%    Licence.txt, included with the software, for details.
+
+%% Set up model parameters
 
   reentry_param;
   make_reentry_data;
@@ -39,6 +61,9 @@
   disp(['This is a demonstration for tracking a reentry vehicle ',...
         'using 1st order EKF and augmented UKF.'])
   disp(' ');
+  
+  
+%% Extended Kalman filter
   
   fprintf('Running EKF...'); 
   % Filtering with EKF
@@ -86,6 +111,10 @@
   ME_ETF = squeeze(SP_ETF(1,1,:)+SP_ETF(2,2,:));
   fprintf('Done!\n');
   
+  
+%% Unscented Kalman filter
+
+  fprintf('Running UKF...'); 
 
   % Initial values and space for (augmented) UKF 
   m = m0;
@@ -94,8 +123,6 @@
   PP_UKF = zeros(size(m,1),size(m,1),size(Y,2));
   VV_UKF = zeros(size(m,1),size(Y,2));
   EE_UKF = zeros(size(m,1),size(Y,2));
-  
-  fprintf('Running UKF...'); 
 
   % Filtering with UKF
   for k=1:size(Y,2)
@@ -155,10 +182,107 @@
   ME_UTF = squeeze(SP_UTF(1,1,:)+SP_UTF(2,2,:));
 
   fprintf('Done!\n');
+
   
+%% Gauss-Hermite Kalman filter
+  
+  fprintf('Running GHKF...'); 
+  
+  % Initial values and space for GHKF
+  m = m0;
+  P = P0;
+  MM_GHKF = zeros(size(m,1),size(Y,2));
+  PP_GHKF = zeros(size(m,1),size(m,1),size(Y,2));
+  VV_GHKF = zeros(size(m,1),size(Y,2));
+  EE_GHKF = zeros(size(m,1),size(Y,2));
+  
+  % Filtering with GHKF
+  for k=1:size(Y,2)
+    [m,P] = ghkf_predict(m,P,func_f,Q,{dt,b0,H0,Gm0,R0},3);
+    [m,P] = ghkf_update(m,P,Y(:,k),func_h,diag([vr va]),{xr,yr},3);
+    MM_GHKF(:,k) = m;
+    PP_GHKF(:,:,k) = P;
+    VV_GHKF(:,k) = diag(P);
+    EE_GHKF(:,k) = (X(:,k) - m).^2;
+  end
+
   %
-  % Plot
+  % Calculate RMSE
   %
+  ghkf_rmse = sqrt(mean(sum((X(1:2,:)-MM_GHKF(1:2,:)).^2)));
+  ME_GHKF = squeeze(PP_GHKF(1,1,:)+PP_GHKF(2,2,:));
+  fprintf('Done!\n')
+
+  fprintf('Running smoother...');
+  %
+  % Smoother
+  %
+  [SM_GHRTS,SP_GHRTS] = ghrts_smooth(MM_GHKF,PP_GHKF,func_f,Q,...
+                          {dt,b0,H0,Gm0,R0},3);
+  ghrts_rmse1 = sqrt(mean(sum((X(1:2,:)-SM_GHRTS(1:2,:)).^2)));
+  ME_GHRTS = squeeze(SP_GHRTS(1,1,:)+SP_GHRTS(2,2,:));
+
+
+  SV_GHRTS = zeros(size(m,1),size(Y,2));
+  SE_GHRTS = zeros(size(m,1),size(Y,2));
+  for k=1:size(Y,2)
+    SV_GHRTS(:,k) = diag(SP_GHRTS(:,:,k));
+    SE_GHRTS(:,k) = (X(:,k) - SM_GHRTS(:,k)).^2;
+  end
+
+  fprintf('Done!\n');
+
+  
+%% Cubature Kalman filter
+  
+  fprintf('Running CKF...'); 
+  
+  % Initial values and space for CKF
+  m = m0;
+  P = P0;
+  MM_CKF = zeros(size(m,1),size(Y,2));
+  PP_CKF = zeros(size(m,1),size(m,1),size(Y,2));
+  VV_CKF = zeros(size(m,1),size(Y,2));
+  EE_CKF = zeros(size(m,1),size(Y,2));
+  
+  % Filtering with CKF
+  for k=1:size(Y,2)
+    [m,P] = ckf_predict(m,P,func_f,Q,{dt,b0,H0,Gm0,R0});
+    [m,P] = ckf_update(m,P,Y(:,k),func_h,diag([vr va]),{xr,yr});
+    MM_CKF(:,k) = m;
+    PP_CKF(:,:,k) = P;
+    VV_CKF(:,k) = diag(P);
+    EE_CKF(:,k) = (X(:,k) - m).^2;
+  end
+
+  %
+  % Calculate RMSE
+  %
+  ckf_rmse = sqrt(mean(sum((X(1:2,:)-MM_CKF(1:2,:)).^2)));
+  ME_CKF = squeeze(PP_CKF(1,1,:)+PP_CKF(2,2,:));
+  fprintf('Done!\n')
+
+  fprintf('Running smoother...');
+  %
+  % Smoother
+  %
+  [SM_CRTS,SP_CRTS] = crts_smooth(MM_CKF,PP_CKF,func_f,Q,...
+                          {dt,b0,H0,Gm0,R0});
+  crts_rmse1 = sqrt(mean(sum((X(1:2,:)-SM_CRTS(1:2,:)).^2)));
+  ME_CRTS = squeeze(SP_CRTS(1,1,:)+SP_CRTS(2,2,:));
+
+
+  SV_CRTS = zeros(size(m,1),size(Y,2));
+  SE_CRTS = zeros(size(m,1),size(Y,2));
+  for k=1:size(Y,2)
+    SV_CRTS(:,k) = diag(SP_CRTS(:,:,k));
+    SE_CRTS(:,k) = (X(:,k) - SM_CRTS(:,k)).^2;
+  end
+
+  fprintf('Done!\n');
+  
+%% Visualize methods
+  
   if ~silent
     aa = 0.02*(-1:0.1:4);
     cx = R0 * cos(aa);
@@ -226,16 +350,88 @@
     title('RMSE of estimating x_5 with UKF and URTS')
     clc;
     disp('RMSE of estimating x_5 with EKF and ERTS is now displayed.');
+    
+    
+    % Plot GHKF estimate
+    plot(xr,yr,'ko',cx,cy,'r-',X(1,:),X(2,:),'g-',...
+         MM_GHKF(1,:),MM_GHKF(2,:),'k--');
+    legend('Radar','Earth','True','Estimate');
+    clc;
+    disp('Filtering result with GHKF is now displayed.');
+    disp(' ');
+    disp('<press any key to see the estimation error of x_1>');    
+    pause;
+  
+    % Error for x_1 with GHKF
+    semilogy(T,EE_UKF(1,:),'g-',T,VV_UKF(1,:),'b--',...
+             T,SE_URTS(1,:),'r-',T,SV_URTS(1,:),'k--');
+    legend('GHKF-RMSE','GHKF-STDE','GHRTS-RMSE','GHRTS-STDE');
+    title('RMSE of estimating x_1 with GHKF and GHRTS')    
+    clc;
+    disp('RMSE of estimating x_1 with GHKF and GHRTS is now displayed.');
+    disp(' ');
+    disp('<press any key to see the estimation error of x_5>');
+    pause;
+  
+    % Error for x_5 with GHKF
+    semilogy(T,EE_GHKF(5,:),'g-',T,VV_GHKF(5,:),'b--',...
+             T,SE_GHRTS(5,:),'r-',T,SV_GHRTS(5,:),'k--');
+    legend('GHKF-RMSE','GHKF-STDE','GHRTS-RMSE','GHRTS-STDE');
+    title('RMSE of estimating x_5 with GHKF and GHRTS')
+    clc;
+    disp('RMSE of estimating x_5 with EKF and GHRTS is now displayed.');
+    
+    
+    % Plot CKF estimate
+    plot(xr,yr,'ko',cx,cy,'r-',X(1,:),X(2,:),'g-',...
+         MM_CKF(1,:),MM_CKF(2,:),'k--');
+    legend('Radar','Earth','True','Estimate');
+    clc;
+    disp('Filtering result with CKF is now displayed.');
+    disp(' ');
+    disp('<press any key to see the estimation error of x_1>');    
+    pause;
+  
+    % Error for x_1 with CKF
+    semilogy(T,EE_UKF(1,:),'g-',T,VV_UKF(1,:),'b--',...
+             T,SE_URTS(1,:),'r-',T,SV_URTS(1,:),'k--');
+    legend('CKF-RMSE','CKF-STDE','CRTS-RMSE','CRTS-STDE');
+    title('RMSE of estimating x_1 with CKF and CRTS')    
+    clc;
+    disp('RMSE of estimating x_1 with CKF and CRTS is now displayed.');
+    disp(' ');
+    disp('<press any key to see the estimation error of x_5>');
+    pause;
+  
+    % Error for x_5 with CKF
+    semilogy(T,EE_CKF(5,:),'g-',T,VV_CKF(5,:),'b--',...
+             T,SE_CRTS(5,:),'r-',T,SV_CRTS(5,:),'k--');
+    legend('CKF-RMSE','CKF-STDE','CRTS-RMSE','CRTS-STDE');
+    title('RMSE of estimating x_5 with CKF and CRTS')
+    clc;
+    disp('RMSE of estimating x_5 with EKF and CRTS is now displayed.');
+
+    
   end
 
+  
+%% Show RMSE errors for each method
+  
   disp(' ');
   disp('RMS errors:');
-  fprintf('EKF-RMSE = %.6f [%.6f]\n',ekf_rmse,sqrt(mean(ME_EKF)));
-  fprintf('ERTS-RMSE = %.6f [%.6f]\n',eks_rmse1,sqrt(mean(ME_ERTS)));
-  fprintf('ETF-RMSE = %.6f [%.6f]\n',eks_rmse2,sqrt(mean(ME_ETF)));
-  fprintf('UKF-RMSE = %.6f [%.6f]\n',ukf_rmse,sqrt(mean(ME_UKF)));
+  fprintf('EKF-RMSE   = %.6f [%.6f]\n',ekf_rmse,sqrt(mean(ME_EKF)));
+  fprintf('ERTS-RMSE  = %.6f [%.6f]\n',eks_rmse1,sqrt(mean(ME_ERTS)));
+  fprintf('ETF-RMSE   = %.6f [%.6f]\n',eks_rmse2,sqrt(mean(ME_ETF)));
+  fprintf('UKF-RMSE   = %.6f [%.6f]\n',ukf_rmse,sqrt(mean(ME_UKF)));
   fprintf('URTS1-RMSE = %.6f [%.6f]\n',uks_rmse1,sqrt(mean(ME_URTS)));
   fprintf('URTS2-RMSE = %.6f [%.6f]\n',uks_rmse1b,sqrt(mean(ME_URTSb)));
-  fprintf('UTF-RMSE = %.6f [%.6f]\n',uks_rmse2,sqrt(mean(ME_UTF)));
+  fprintf('UTF-RMSE   = %.6f [%.6f]\n',uks_rmse2,sqrt(mean(ME_UTF)));
 
+  % Cubature methods
+  fprintf('GHKF-RMSE  = %.6f [%.6f]\n',ghkf_rmse,sqrt(mean(ME_GHKF)));
+  fprintf('GHRTS-RMSE = %.6f [%.6f]\n',ghrts_rmse1,sqrt(mean(ME_GHRTS)));
+  fprintf('CKF-RMSE   = %.6f [%.6f]\n',ckf_rmse,sqrt(mean(ME_CKF)));
+  fprintf('CRTS-RMSE  = %.6f [%.6f]\n',crts_rmse1,sqrt(mean(ME_CRTS)));
+  
+  
   
