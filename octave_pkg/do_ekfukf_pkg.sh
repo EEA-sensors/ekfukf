f65a5f9a150dfa76d2d4f240756051e92c2f480f
JuanPi Carbajal <ajuanpi+dev@gmail.com>
2018-06-30 03:16:22 +0200

    maint: minor fix to scrpt

--- octave_pkg/do_ekfukf_pkg.sh
+++ octave_pkg/do_ekfukf_pkg.sh
@@ -5,6 +5,6 @@ cp ../LICENSE.txt COPYING
 # INDEX
 python3 do_index.py
 # PACKAGE
-python3 do_pkg.py -i .. -o ekfukf_of &&\
+python3 do_pkg.py -i .. -o ekfukf_of && \
 # remove created files
 rm COPYING INDEX
